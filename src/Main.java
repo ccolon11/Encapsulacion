db0d6a5d3bdd990ab3fdd62a13a745173cb3a04f
Carlos Colón <carloscolon_7@hotmail.com>
2022-12-25 15:02:32 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.setNombre ("Eduardo");
+        persona.setEdad (18);
+        persona.setTelefono(1234);
+
+        System.out.println(persona.getNombre());
+        System.out.println(persona.getEdad());
+        System.out.println(persona.getTelefono());
+    }
+    static class Persona {
+        private String nombre;
+        private int edad;
+        private int telefono;
+        public void setNombre(String nombre) {
+            this.nombre = nombre;}
+        public String getNombre ()
+        { return this.nombre; }
+
+        public void setEdad(int edad) {
+            this.edad = edad;}
+        public int getEdad ()
+        { return this.edad; }
+
+        public void setTelefono(int telefono) {
+            this.telefono = telefono;}
+        public int getTelefono ()
+        { return this.telefono; }
+
+    }
+}
